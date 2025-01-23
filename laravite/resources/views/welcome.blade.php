938d8e795365f3873d9d7a119066598dc6af9cdc
Arturo Zvezda <iskra.zvezda@gmail.com>
2025-01-23 01:58:21 -0600

    Tailwind base configuration

--- laravite/resources/views/welcome.blade.php
+++ laravite/resources/views/welcome.blade.php
@@ -10,5 +10,13 @@
         <div id="app">
             <h1>¡Laravel y Vite funcionando juntos!</h1>
         </div>
+
+        <h1 class="text-3xl font-bold text-blue-600">¡Laravel y Tailwind CSS funcionando juntos!</h1>
+        <p class="mt-4 text-gray-600">Este texto está estilizado con Tailwind CSS.</p>
+        <button class="mt-6 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700">
+            Botón con Tailwind
+        </button>
+
+
     </body>
 </html>

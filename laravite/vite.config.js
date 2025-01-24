1193560c6e61be9855ed27d6bcf01aeca6674252
Arturo Zvezda <iskra.zvezda@gmail.com>
2025-01-24 08:35:01 -0600

    Adding missing import on vite config

--- laravite/vite.config.js
+++ laravite/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
+import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
 
 export default defineConfig({
